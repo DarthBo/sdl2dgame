9184451d800b130c2f595bac7adf4c62249f2690
Tibault Damman <tibault.damman@gmail.com>
2014-12-18 13:58:22 +0100

    Improved input->velocity code

--- src/game.cpp
+++ src/game.cpp
@@ -12,6 +12,12 @@ int Game::Start()
     Player player(sdl.GetPath("res") + "front.png", win, 320, FLOOR);
     player.ModulateColor(255, 0, 0);
 
+    //Calculate player center
+    int pMidX, pMidY;
+    player.QueryDimensions(&pMidX, &pMidY);
+    pMidX /= 2;
+    pMidY /= 2;
+
     SDL_Event e;
     bool quit = false;
     while (!quit)
@@ -22,25 +28,25 @@ int Game::Start()
             {
                 quit = true;
             }
-            if (e.type == SDL_KEYDOWN && e.key.repeat == 0)
+            if (e.type == SDL_KEYDOWN)
             {
                 switch (e.key.keysym.scancode)
                 {
                     case SDL_SCANCODE_LEFT:
                     case SDL_SCANCODE_A:
                         //go left
-                        player.velX -= VEL;
+                        player.isLeft = true;
                         break;
                     case SDL_SCANCODE_RIGHT:
                     case SDL_SCANCODE_D:
                         //go right
-                        player.velX += VEL;
+                        player.isRight = true;
                         break;
                     case SDL_SCANCODE_UP:
                     case SDL_SCANCODE_W:
                         //go up
-                        if (player.velY == 0) //todo replace with if onground
-                            player.velY -= JMP;
+                        if (!player.isFalling & !player.isJumping)
+                            player.isJumping = true;
                         break;
                     case SDL_SCANCODE_DOWN:
                     case SDL_SCANCODE_S:
@@ -55,19 +61,19 @@ int Game::Start()
                         break;
                 }
             }
-            else if (e.type == SDL_KEYUP && e.key.repeat == 0)
+            else if (e.type == SDL_KEYUP)
             {
                 switch (e.key.keysym.scancode)
                 {
                     case SDL_SCANCODE_LEFT:
                     case SDL_SCANCODE_A:
                         //go left
-                        player.velX += VEL;
+                        player.isLeft = false;
                         break;
                     case SDL_SCANCODE_RIGHT:
                     case SDL_SCANCODE_D:
                         //go right
-                        player.velX -= VEL;
+                        player.isRight = false;
                         break;
                     case SDL_SCANCODE_UP:
                     case SDL_SCANCODE_W:
@@ -89,12 +95,21 @@ int Game::Start()
             }
         }
 
-        //Calculate position
-        int pMidX, pMidY;
-        player.QueryDimensions(&pMidX, &pMidY);
-        pMidX /= 2;
-        pMidY /= 2;
+        //Player velocity
+        if (player.isJumping)
+        {
+            player.velY -= JMP;
+            player.isJumping = false;
+            player.isFalling = true;
+        }
 
+        player.velX = 0;
+        if (player.isLeft)
+            player.velX -= VEL;
+        if (player.isRight)
+            player.velX += VEL;
+
+        //Player position
         player.posX += player.velX;
         if (player.posX + pMidX < 0)
             player.posX = SCREEN_WIDTH - pMidX;
@@ -107,7 +122,7 @@ int Game::Start()
             player.posY = -pMidY;
 
         //gravity
-        if (player.posY < FLOOR)
+        if (player.isFalling)
             player.velY += 1;
 
         //hardcoded floor
@@ -115,6 +130,7 @@ int Game::Start()
         {
             player.posY = FLOOR;
             player.velY = 0;
+            player.isFalling = false;
         }
 
         //Render the scene
--- src/game.h
+++ src/game.h
@@ -19,8 +19,15 @@ class Player : public sdl::Texture
 {
 public:
     Player(const std::string &file, const sdl::Window &window, int x, int y)
-        : sdl::Texture(file, window), posX(x), posY(y), velX(0), velY(0) {}
+        : sdl::Texture(file, window),
+          isLeft(false), isRight(false), isFalling(false), isJumping(false),
+          posX(x), posY(y), velX(0), velY(0) {}
     ~Player() {}
+
+    bool isLeft;
+    bool isRight;
+    bool isFalling;
+    bool isJumping;
 
     int posX, posY;
     int velX, velY;
